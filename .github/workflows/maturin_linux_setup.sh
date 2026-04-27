c3d1665c063bc7e51eceff0280eaa86377f3eca2
jackbmontgomery <jackbmontgomery2001@gmail.com>
2026-04-27 21:51:00 +0100

    ci: updating llvm version to 20 everywhere

--- .github/workflows/maturin_linux_setup.sh
+++ .github/workflows/maturin_linux_setup.sh
@@ -5,19 +5,19 @@ if [ -f "/etc/debian_version" ]; then
     apt-get install -y wget lsb-release software-properties-common gnupg
     wget https://apt.llvm.org/llvm.sh
     chmod +x llvm.sh
-    ./llvm.sh 17
-    apt-get install -y libpolly-17-dev libzstd-dev zstd libncurses-dev zlib1g-dev
-    export LLVM_DIR=/usr/lib/llvm-17
-    export LLVM_SYS_170_PREFIX=/usr/lib/llvm-17
+    ./llvm.sh 20
+    apt-get install -y libpolly-20-dev libzstd-dev zstd libncurses-dev zlib1g-dev
+    export LLVM_DIR=/usr/lib/llvm-20
+    export LLVM_SYS_200_PREFIX=/usr/lib/llvm-20
 
     echo Installing SparseSuite via apt
     apt-get install -y libsuitesparse-dev
 else
     echo Installing LLVM via yum
     yum update -y
-    yum -y install llvm-devel-17.0.6 clang-devel-17.0.6 libzstd-devel zstd ncurses-devel zlib-devel libffi libffi-devel
+    yum -y install llvm-devel-20.1.0 clang-devel-20.1.0 libzstd-devel zstd ncurses-devel zlib-devel libffi libffi-devel
     export LLVM_DIR=/usr
-    export LLVM_SYS_170_PREFIX=/usr
+    export LLVM_SYS_200_PREFIX=/usr
 
     echo Installing SparseSuite via yum
     yum install -y suitesparse-devel
